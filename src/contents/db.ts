c58800fcd10069b361d4af343953def90e1882f9
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2025-06-24 22:49:15 -0600

    Added: components and routes

--- src/contents/db.ts
+++ src/contents/db.ts
@@ -1,6 +1,13 @@
-import type { SkillSetProps, NavItemProps } from "../interfaces/interface";
+import type {
+  SkillSetProps,
+  NavItemProps,
+  HorizontalCardProps,
+  Social,
+  TerminalProps,
+  ProjectProps,
+} from "../interfaces/interface";
 
-export const Skills: SkillSetProps[] = [
+export const skills: SkillSetProps[] = [
   {
     tittle: "Frontend Development",
     skills: [
@@ -38,7 +45,7 @@ export const NavItems: NavItemProps[] = [
   },
   {
     label: "Skills",
-    href: "/",
+    href: "/skills",
     position: "center",
   },
   {
@@ -48,12 +55,98 @@ export const NavItems: NavItemProps[] = [
   },
   {
     label: "Projects",
-    href: "/",
+    href: "/projects",
     position: "center",
   },
   {
     label: "Contact Me",
     href: "/",
     position: "right",
+  },
+];
+
+export const HorizontalCardList: HorizontalCardProps[] = [
+  {
+    tittle: "About me 🧑🏾‍💻",
+    description:
+      "I'm in the final year of my Systems Engineering degree at the National Autonomous University of Honduras. My academic journey began with a technical high school education in Computer Science, which laid a strong foundation in IT and problem-solving. I'm fluent in both Spanish and English, and I also have skills in graphic design. I hold a CCNA 1 certification in networking fundamentals, and I completed the Oracle Next Education (ONE) programming course offered by Alura Latam, where I gained practical experience in software development and industry best practices.",
+  },
+  {
+    tittle: "Academic Background 📚",
+    description:
+      "I'm in the final year of my Systems Engineering degree at the National Autonomous University of Honduras. My academic journey began with a technical high school education in Computer Science, which laid a strong foundation in IT and problem-solving. I'm fluent in both Spanish and English, and I also have skills in graphic design. I hold a CCNA 1 certification in networking fundamentals, and I completed the Oracle Next Education (ONE) programming course offered by Alura Latam, where I gained practical experience in software development and industry best practices.",
+  },
+  {
+    tittle: "Experience 📐",
+    description:
+      "I'm in the final year of my Systems Engineering degree at the National Autonomous University of Honduras. My academic journey began with a technical high school education in Computer Science, which laid a strong foundation in IT and problem-solving. I'm fluent in both Spanish and English, and I also have skills in graphic design. I hold a CCNA 1 certification in networking fundamentals, and I completed the Oracle Next Education (ONE) programming course offered by Alura Latam, where I gained practical experience in software development and industry best practices.",
+  },
+];
+
+export const SocialList: Social[] = [
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/ric.null",
+    icon: "https://s.magecdn.com/social/tc-instagram.svg",
+  },
+  {
+    name: "facebook",
+    url: "https://www.facebook.com/ric.null",
+    icon: "https://s.magecdn.com/social/tc-facebook.svg",
+  },
+  {
+    name: "github",
+    url: "https://www.github.com/ricjpg",
+    icon: "https://s.magecdn.com/social/tc-github.svg",
+  },
+  {
+    name: "linkedIn",
+    url: "https://www.linkedin.com/in/ricnull",
+    icon: "https://s.magecdn.com/social/tc-linkedin.svg",
+  },
+];
+
+export const terminalList: TerminalProps[] = [
+  {
+    title: "About me 🧑🏾‍💻",
+    description:
+      "I'm in the final year of my Systems Engineering degree at the National Autonomous University of Honduras. My academic journey began with a technical high school education in Computer Science, which laid a strong foundation in IT and problem-solving. I'm fluent in both Spanish and English, and I also have skills in graphic design. I hold a CCNA 1 certification in networking fundamentals, and I completed the Oracle Next Education (ONE) programming course offered by Alura Latam, where I gained practical experience in software development and industry best practices.",
+  },
+  {
+    title: "Academic Background 📚",
+    description:
+      "I'm in the final year of my Systems Engineering degree at the National Autonomous University of Honduras. My academic journey began with a technical high school education in Computer Science, which laid a strong foundation in IT and problem-solving. I'm fluent in both Spanish and English, and I also have skills in graphic design. I hold a CCNA 1 certification in networking fundamentals, and I completed the Oracle Next Education (ONE) programming course offered by Alura Latam, where I gained practical experience in software development and industry best practices.",
+  },
+  {
+    title: "Experience 📐",
+    description:
+      "I'm in the final year of my Systems Engineering degree at the National Autonomous University of Honduras. My academic journey began with a technical high school education in Computer Science, which laid a strong foundation in IT and problem-solving. I'm fluent in both Spanish and English, and I also have skills in graphic design. I hold a CCNA 1 certification in networking fundamentals, and I completed the Oracle Next Education (ONE) programming course offered by Alura Latam, where I gained practical experience in software development and industry best practices.",
+  },
+];
+
+export const projectsList: ProjectProps[] = [
+  {
+    slug: "/prosene",
+    title: "PROSENE",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+    img: "prosene.png",
+    href: "/projects/prosene",
+  },
+  {
+    slug: "/xatruch",
+    title: "Xatruch",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters",
+    img: "plane.jpg",
+    href: "/projects/xatruch",
+  },
+  {
+    slug: "/classifier",
+    title: "Cats and dogs classifier",
+    description:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there",
+    img: "classifier.png",
+    href: "/projects/classifier",
   },
 ];
--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -13,3 +13,31 @@ export interface NavItemProps {
   href: string;
   position: "left" | "center" | "right";
 }
+
+export interface HorizontalCardProps {
+  tittle: string;
+  description: string;
+}
+
+export interface DataPage {
+  slug: string;
+}
+
+export interface Social {
+  name: string;
+  url: string;
+  icon: string;
+}
+
+export interface TerminalProps {
+  title: string;
+  description: string;
+}
+
+export interface ProjectProps {
+  slug: string;
+  title: string;
+  img?: string;
+  description: string;
+  href?: string;
+}
